9f9e079630eb77cc666fc3835d20b99809b774b2
Namgyu Ho <itsnamgyu@gmail.com>
2018-12-29 17:20:32 +0900

    Simplify structure
    Move settings, urls, wsgi to base directory of Django project

--- aws_interface/settings.py
+++ aws_interface/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 
 # Quick-start development settings - unsuitable for production
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'aws_interface.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'aws_interface.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
