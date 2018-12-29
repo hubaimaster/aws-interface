969fee45d65f1e2bc33a46d82f336a4a2a756a02
Namgyu Ho <itsnamgyu@gmail.com>
2018-12-29 13:04:31 +0900

    add Python modules as requirements.txt

--- aws_interface/python3-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/kchdully/Desktop/aws-interface/aws_interface/python3-env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
