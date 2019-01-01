62f2b5380592c560690492d7d72c068712aedff9
Namgyu Ho <itsnamgyu@gmail.com>
2019-01-01 16:54:22 +0900

    Bug fix
    Simple typo

--- aws_interface/dashboard/views/login.py
+++ aws_interface/dashboard/views/login.py
@@ -31,8 +31,9 @@ class Login(View, DashboardView):
                 access_key = aes.decrypt(user.c_aws_access_key)
                 secret_key = aes.decrypt(user.c_aws_secret_key)
                 boto3_session = service.get_boto3_session(access_key, secret_key)
+                print(boto3_session)
                 self.set_login(request, True, boto3_session)
                 return redirect('index')
             else:
-                self.add_alert('비밀번호가 틀렸습니다')
+                self.add_alert(request, '비밀번호가 틀렸습니다')
                 return redirect('index')
