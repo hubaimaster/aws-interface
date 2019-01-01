b2c6d7612fa765b8f9f5822b9109f4c8197522a4
Namgyu Ho <itsnamgyu@gmail.com>
2019-01-01 20:15:51 +0900

    Use LoginRequiredMixin for login check
    Look at the code!

--- aws_interface/settings.py
+++ aws_interface/settings.py
@@ -72,6 +72,8 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'wsgi.application'
 
+LOGIN_URL = 'login/'
+LOGIN_REDIRECT_URL = 'login/'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
