e276a2571b9360d3ae579c597734640292d39047
kimchanghwan <kchdully@sogang.ac.kr>
2019-02-04 12:45:38 +0900

    Update API method

--- aws_interface/core/api.py
+++ aws_interface/core/api.py
@@ -137,25 +137,28 @@ class DatabaseAPI(API):
 
     # Service
     def create_item(self, partition, item, read_permissions=['all'], write_permissions=['all']):
-        return self.service_controller.create_item(partition, item, read_permissions, write_permissions)
+        return self.service_controller.create_item(self.recipe_controller.to_json(),
+                                                   partition, item, read_permissions, write_permissions)
 
     def update_item(self, item_id, item, read_permissions=['all'], write_permissions=['all']):
-        return self.service_controller.update_item(item_id, item, read_permissions, write_permissions)
+        return self.service_controller.update_item(self.recipe_controller.to_json(),
+                                                   item_id, item, read_permissions, write_permissions)
 
     def update_item_field(self, item_id, field_name, field_value):
-        return self.service_controller.update_item_field(item_id, field_name, field_value)
+        return self.service_controller.update_item_field(self.recipe_controller.to_json(),
+                                                         item_id, field_name, field_value)
 
     def get_item(self, item_id):
-        return self.service_controller.get_item(item_id)
+        return self.service_controller.get_item(self.recipe_controller.to_json(), item_id)
 
     def delete_item(self, item_id):
-        raise self.service_controller.delete_item(item_id)
+        raise self.service_controller.delete_item(self.recipe_controller.to_json(), item_id)
 
     def get_items(self, partition):
-        return self.service_controller.get_items(partition)
+        return self.service_controller.get_items(self.recipe_controller.to_json(), partition)
 
     def get_item_count(self, partition):
-        return self.service_controller.get_item_count(partition)
+        return self.service_controller.get_item_count(self.recipe_controller.to_json(), partition)
 
     def search_items(self, query):
         raise NotImplementedError()
