cd20b26bfc5fc88efe7e715cacb2e597a858a4f8
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-13 07:42:39 +0000

    Add static files settings for deployment

--- aws_interface/settings.py
+++ aws_interface/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = "/var/www/aws-interface.com/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
