4d3b1be33e4644091611167fa975d577f3e1e4e2
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-18 16:01:06 +0900

    Add underscore to indicate private function

--- aws_interface/cloud/response.py
+++ aws_interface/cloud/response.py
@@ -1,6 +1,6 @@
 
 
-def get_header(content_type='application/json'):
+def _get_header(content_type='application/json'):
     api_gateway_response_header = {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Requested-With',
@@ -12,7 +12,7 @@ def get_header(content_type='application/json'):
 
 class Response(dict):
     def __init__(self, body, content_type='application/json', status_code=200):
-        header = get_header(content_type)
+        header = _get_header(content_type)
         self['statusCode'] = status_code
         self['header'] = header
         self['body'] = body
