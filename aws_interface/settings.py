55946e6a1040b83b963556de907dc9b4035885ed
kimchanghwan <hubaimaster@gmail.com>
2019-02-20 12:56:51 +0900

    Update secret key setting

--- aws_interface/settings.py
+++ aws_interface/settings.py
@@ -9,18 +9,22 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import json
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.dirname(BASE_DIR)
+SECRETS_DIR = os.path.join(ROOT_DIR, 'secret')
+SECRETS_BASE = os.path.join(SECRETS_DIR, 'base.json')
+secrets_base = json.loads(open(SECRETS_BASE, 'rt').read())
 
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = secrets_base['SECRET_KEY']
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ')o_-=e6rti-w@q(9ngcqgjya%h@x@%*be0lx(-f_e0)!j%p@=*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
