be2306d8ccc595f8f3a64f86f2f2428257f89326
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-20 21:31:50 +0900

    Reset initialization status on startup
    If the server was interrupted while a Recipe was getting
    initialized, indicate that the initialization has failed.

--- aws_interface/dashboard/apps.py
+++ aws_interface/dashboard/apps.py
@@ -1,5 +1,19 @@
 from django.apps import AppConfig
+from .models import Recipe
+
+
+def reset_recipe_initialization():
+    """
+    If the server was interrupted while a Recipe was getting
+    initialized, indicate that the initialization has failed.
+    :return:
+    """
+    Recipe.objects.all().filter(init_status=Recipe.INIT_PROGRESS).update(init_status=Recipe.INIT_FAILED)
 
 
 class DashboardConfig(AppConfig):
     name = 'dashboard'
+
+    def ready(self):
+        reset_recipe_initialization()
+
