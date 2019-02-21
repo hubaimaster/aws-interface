e13cfdcb42d20db4e934ea0c86b168507685107d
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-21 20:59:05 +0900

    Add models to admin for troubleshooting

--- aws_interface/dashboard/admin.py
+++ aws_interface/dashboard/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import User
+from .models import User, App, Recipe
 
 admin.site.register(User, UserAdmin)
+admin.site.register(App)
+admin.site.register(Recipe)
