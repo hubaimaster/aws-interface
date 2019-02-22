88255fd06554b10364580325edcc66311bd7885a
Namgyu Ho <itsnamgyu@gmail.com>
2019-02-22 12:22:13 +0900

    Remove collectstatic from dev script

--- scripts/refresh.sh
+++ scripts/refresh.sh
@@ -1,5 +1,4 @@
 git pull;
 python aws_interface/manage.py makemigrations;
 python aws_interface/manage.py migrate;
-python aws_interface/manage.py collectstatic --noinput;
 python aws_interface/manage.py test
