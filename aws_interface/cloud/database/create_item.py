097b300a5ca756726cbcd62b044c2f69c961ae63
kimchanghwan <hubaimaster@gmail.com>
2019-02-24 20:18:45 +0900

    Update create_item.py

--- aws_interface/cloud/database/create_item.py
+++ aws_interface/cloud/database/create_item.py
@@ -34,7 +34,7 @@ def do(data, boto3):
 
     read_groups.append('admin')
     write_groups.append('admin')
-    
+
     item['read_groups'] = read_groups
     item['write_groups'] = write_groups
     item['owner'] = user_id
