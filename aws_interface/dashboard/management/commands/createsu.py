840c030f9e14df551e7ec8057671cf88ee7bb61a
kimchanghwan <hubaimaster@gmail.com>
2019-05-03 13:44:26 +0900

    Add Elasticbeanstalk

--- aws_interface/dashboard/management/commands/createsu.py
+++ aws_interface/dashboard/management/commands/createsu.py
@@ -1,8 +1,8 @@
 from django.core.management.base import BaseCommand
-from django.contrib.auth.models import User
+from dashboard.models import User
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'kchdully@gmail.com', 'kch1234')
+            User.objects.create_superuser('kchdully@gmail.com', 'kch1234')
