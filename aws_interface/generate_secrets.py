2037a46b2814d994613b0eb7d11eee528c767162
kimchanghwan <hubaimaster@gmail.com>
2019-05-09 12:34:26 +0900

    Update generate_secrets.py

--- aws_interface/generate_secrets.py
+++ aws_interface/generate_secrets.py
@@ -16,14 +16,18 @@ if __name__ == '__main__':
     data['ADMIN_EMAIL'] = input('ADMIN_EMAIL: ')
     data['ADMIN_PASSWORD'] = input('ADMIN_PASSWORD: ')
 
-    data["DB_ENGINE"] = "django.db.backends.mysql"
+    DB_ENGINE = input('DB_ENGINE (mysql, sqlite3) : ')
+    data["DB_ENGINE"] = "django.db.backends.{}".format(DB_ENGINE)
     print('Using DB_ENGINE as {}'.format(data["DB_ENGINE"]))
 
-    data["DB_NAME"] = input('DB_NAME: ')
-    data["DB_USER"] = input('DB_USER: ')
-    data["DB_PASSWORD"] = input('DB_PASSWORD: ')
-    data["DB_HOST"] = input('DB_HOST: ')
-    data["DB_PORT"] = input('DB_PORT: ')
+    if DB_ENGINE == 'sqlite3':
+        data["DB_NAME"] = input('DB_NAME: ')
+    else:
+        data["DB_NAME"] = input('DB_NAME: ')
+        data["DB_USER"] = input('DB_USER: ')
+        data["DB_PASSWORD"] = input('DB_PASSWORD: ')
+        data["DB_HOST"] = input('DB_HOST: ')
+        data["DB_PORT"] = input('DB_PORT: ')
 
     data["AWS_ACCESS_KEY"] = input('AWS_ACCESS_KEY: ')
     data["AWS_SECRET_KEY"] = input('AWS_SECRET_KEY: ')
@@ -31,3 +35,4 @@ if __name__ == '__main__':
     base_json = os.path.join(secret_dir, 'base.json')
     with open(base_json, 'w') as f:
         json.dump(data, f)
+
