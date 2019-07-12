78d1d41c659e18abe3e60451a323bf725a715cc1
Naruu <esara2021@gmail.com>
2019-07-12 10:27:46 -0400

    Implemented async testing on auth, database, log. Further implementation needed on logic and storage

--- aws_interface/sdk/nodejs/test/authTest.js
+++ aws_interface/sdk/nodejs/test/authTest.js
@@ -4,74 +4,83 @@ const Client = require('../aws_interface.js');
 const assert = require("assert");
 
 var client = new Client.Client();
-const new_email = "test12@email.com";
-const new_password = "test12password";
+const new_email = "test21@email.com";
+const new_password = "test21password";
 const test_email = "ttest@gmail.com";
 const test_password = "ttestpassword";
 const fb_token = "";
 const user_id = "jK5iK6UgYnTdaRhow5yvAT";
 
 describe('Auth Test', function() {
+    //Set timeout of 7 seconds for async testing.
+    this.timeout(7000);
 
     //Register new user with [new_email], [new_password]
     //Function is commented since it gets already registered user when repeated.
     /*
-    it('test authRegister', client.authRegister(new_email, new_password, {},function(body) {
-        assert.equal(body['item']['email'] == test_email)
-    }));
-   */
+    it('test authRegister', function(){
+         client.authRegister(new_email, new_password, {})
+             .then(function(body) {
+                 assert.equal(body['item']['email'] == test_email);
+             });
+     });
+    */
+
     // Login and logout
     it('test login and logout', function(done){
-        client.authLogin(test_email, test_password, function(body) {
-            client.authLogout(function(response) {
-                assert.ok(response['success']);
+        client.authLogin(test_email, test_password)
+            .then(function(){
+                client.authLogout().then(function(response) {
+                        assert.ok(response['success']);
+                        done();
+                    });
             });
-        });
-        done();
     });
 
-/*
+
     //Login with facebook access token [fb_token]
     it('test authLoginFacebook', function(done){
-        client.authLoginFacebook(fb_token, function(body){
-            assert.ok(body['session_id']);
-
-        });
-        done();
-    });
-
- */
-    //Get user information with [user_id]
-    it('test authGetUser', function(done){
-        client.authLogin(test_email, test_password, function(body) {
-            client.authGetUser(user_id, function(response) {
-                assert.equal(response['item']['email'], test_email);
-                assert.equal(response['item']['id'], user_id);
+        client.authLoginFacebook(fb_token)
+            .then(function(body){
+                assert.ok(body['session_id']);
+                done();
             });
         });
-        done();
+    });
+
+
+    //Get user information with [user_id]
+    it('test authGetUser', function(done){
+        //Extend timeout to 10 seconds
+        this.timeout(10000);
+        client.authLogin(test_email, test_password)
+            .then(function(body) {
+                client.authGetUser(user_id)
+                    .then(function(response){
+                        assert.equal(response['item']['email'], test_email);
+                        assert.equal(response['item']['id'], user_id);
+                        done();
+                    });
+            });
     });
 
     //Get a list of users
     it('test authGetUsers', function(done){
-        client.authLogin(test_email, test_password, function(body) {
-            client.authGetUsers(null, function(response) {
-                assert.ok(response['items']);
+        client.authLogin(test_email, test_password)
+            .then(function(body) {
+                client.authGetUsers(null,)
+                    .then(function(response) {
+                        assert.ok(response['items']);
+                        done();
+                    });
             });
-        });
-        done();
     });
 
     //Guest login
     it('test authGuest', function(done){
-        client.authLogin(test_email, test_password, function(body) {
-            client.authGuest(null, function(response) {
-                assert.ok(response['session_id']);
-            });
-        });
-        done();
+            client.authGuest(null)
+                .then(function(response) {
+                    assert.ok(response['session_id']);
+                    done();
+                });
     });
-
-});
-
-
--- aws_interface/sdk/nodejs/test/logTest.js
+++ aws_interface/sdk/nodejs/test/logTest.js
@@ -1,5 +1,4 @@
 // logTest.js
-
 const Client = require('../aws_interface.js');
 const assert = require("assert");
 
@@ -9,20 +8,33 @@ const test_password = "ttestpassword";
 const test_source = "test-source";
 const test_event = "test-event-name";
 const test_param = "test-param";
-
+const log_owner = "user";
 
 describe('Log Test', function() {
+    //Set timeout of 7 seconds for async testing.
+    this.timeout(7000);
 
     //test create log with [test_source], [test_event], [test_param]
-    //Currently forbidden request error
-    /*
-    it('test logCreateLog', function(done) {
-        client.authLogin(test_email, test_password, function (body) {
-            client.logCreateLog(test_source, test_event, test_param, function (response) {
-                assert.ok(response['success']);
-            });
-            done();
+    it('test logCreateLog', function(done){
+        client.authLogin(test_email, test_password)
+        .then(function(){
+            client.logCreateLog(test_source, test_event, test_param)
+                .then(function(response){
+                    assert.ok(response['success']);
+                    done(); // Signal that callback is done, so that finish the unit test.
+                });
         });
     });
-     */
+
+    //test get log with [test_source], [test_event], [test_param]
+    it('test logGetLogs', function(done){
+        client.authLogin(test_email, test_password)
+            .then(function(){
+                client.logGetLogs(test_source, test_event, log_owner, 0, false)
+                    .then(function(response){
+                        assert.ok(response['items']);
+                        done(); // Signal that callback is done, so that finish the unit test.
+                    });
+            });
+    });
 });
