f37701bf458bbc0e1fbdc2ffe2dbdb37994d24c5
kimchanghwan <hubaimaster@gmail.com>
2020-04-03 17:30:23 +0900

    Add email sending function

--- aws_interface/cloud/permission.py
+++ aws_interface/cloud/permission.py
@@ -150,6 +150,7 @@ class Permission:
             get_email_providers = 'run:cloud.notification.get_email_providers'
             update_email_provider = 'run:cloud.notification.update_email_provider'
             send_email = 'run:cloud.notification.send_email'
+            send_email2 = 'run:cloud.notification.send_email2'
             send_sms = 'run:cloud.notification.send_sms'
 
     default_user_permissions = [
